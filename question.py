0915d9cd89151c8581f273381e28dfa2a5645992
Rafghi <202419810012@ifba.edu.br>
2025-05-19 11:46:11 -0300

    Update question.py

--- question.py
+++ question.py
@@ -1,36 +1,57 @@
+
+#Entrada 1 - Cadastro de Uma Única Reserva
+
 import sys
 
-nome = input("Digite o seu nome:")
-
+nome = input("Digite o nome do responsável pela reserva:")
+ 
 if nome == "":
     print("Nome inválido! Nome vazio!")
     sys.exit()
 
-dia_in = int(input("Digite o dia do seu check_in: "))
+check_in_dia = int(input("Digite o dia de check-in:"))
 
-if dia_in > 31:
-    print("Dia inválido! Esse dia não existe!")
+if check_in_dia > 31 or check_in_dia < 1:
+    print("Erro! Dia inválido!")
     sys.exit()
 
-mes_in = int(input("Digite o mês do seu check_in: "))
+check_in_mês = int(input("Digite o mês de check-in:"))    
 
-if mes_in > 12:
-    print("Mês inválido! Esse mês não existe")
+if check_in_mês > 12 or check_in_mês < 0:
+    print("Erro! Mês inválido")
     sys.exit()
 
-ano_in = int(input("Digite o ano do seu check_in: "))
+check_in_ano = int(input("Digite o ano de check-in:"))
 
-dia_out = int(input("Digite o dia do seu check_out: "))
+check_out_dia = int(input("Digite o dia de ckeck-out:"))
 
-if dia_out > 31:
-    print("Dia inválido! Esse dia não existe!")
+if check_out_dia > 31 or check_out_dia < 1:
+    print("Erro! Dia inválido")
     sys.exit()
 
-mes_out = int(input("Digite o mês do seu check_out: "))
+check_out_mês = int(input("Digite o mês de check-out:"))
 
-if mes_out > 12:
-    print("Mês inválido! Esse mês não existe")
+if check_out_mês > 12 or check_out_mês:
+    print("Erro! Mês inválido")
+    sys.exit
+    
+check_out_ano = int(input("Digite o ano de check-out:"))
+
+if check_in_ano == check_out_ano:
+    check_in_mês < check_out_mês
+    print("Erro! Não há como o mês de check-out ser menor que o de check-in")
+    
+
+if check_in_ano > check_out_ano:
+    print("Erro! Não há como a data de check-in ser maior que a de check-out")
     sys.exit()
-ano_out = int(input("Digite o ano do seu check_out: ")) 
 
-quarto = input("Digite o tipo de quarto (Stardad, Premium, Luxo):")
+
+ 
+quarto = input("Digite o tipo de quarto que deseja (Standard, Premium, Luxo):")
+
+if quarto != "Standard" and quarto != "Premium" and quarto != "Luxo":
+    print("Erro! O tipo de quarto deve ser Standard, Premium ou Luxo")    
+
+out_mês_dias = check_out_mês * 30
+total_meses = check_out_mês - check_in_mês    
