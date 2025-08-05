45eca69060befbe5c38286cfd316a4d6bc413feb
Rafghi <202419810012@ifba.edu.br>
2025-08-05 10:17:45 -0300

    Update question.py

--- question.py
+++ question.py
@@ -1,3 +1,5 @@
+# ENTREGA 1:
+
 import sys
 from datetime import datetime
 
@@ -13,25 +15,32 @@ if nome == "":
 
 formato = "%d/%m/%Y"
 
-data = input("Digite a data de Check-In (No modelo dd/mm/aaaa): ")
-data = datetime.strptime(data, formato).date()
+check_in = input("Digite a data de Check-In (No modelo dd/mm/aaaa): ")
+check_in = datetime.strptime(check_in, formato).date()
 
-data2 = input("Digite a data de Check-Out (No modelo dd/mm/aaaa): ")
-data2 = datetime.strptime(data2, formato).date()
+check_out = input("Digite a data de Check-Out (No modelo dd/mm/aaaa): ")
+check_out = datetime.strptime(check_out, formato).date()
 
-diferença = data - data2
+if check_out < check_in:
+    print("O Check-out não pode ser maior que o Check-in! Faça novamente")
+    sys.exit()
 
-print(data)
-print(data2)
-print(diferença.days)
+diárias = check_out - check_in
 
 # TIPO DE QUARTO:
 
 quarto = input("Digite o tipo de quarto desejado (Standard, Premium ou Luxo): ")
 
-if quarto == "Standard":
+if quarto == "Standard" or "standard":
     diaria = int(100)
-elif quarto == "Premium":
+elif quarto == "Premium" or "premium":
     diaria = int(180)
-elif quarto == "Luxo":
+elif quarto == "Luxo" or "luxo":
     diaria = int(250)
+else:
+    print("Este quarto não existe!")
+    sys.exit()
+
+preço = diaria * diárias.days
+
+print(f"Olá {nome}! Seu Check-in está marcado para o dia {check_in} e seu Check-out está marcado para o dia {check_out}. Ao total, você ficará {diárias.days} dias conosco! Você ficará no quarto {quarto}. Ao total, o preço será R${preço}.")
